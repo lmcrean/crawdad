29a5b07c2878429fc948ac52c9c7a4b6b1ecdf3f
Your Name <lmcrean@gmail.com>
2025-03-18 07:06:30 +0000

    Rename test view from test_supabase_config to test_database_config, updating the configuration check to reflect database settings instead of Supabase details.

--- api-isolated/api_app/views/test_supabase_config.py
+++ api-isolated/api_app/views/test_supabase_config.py
@@ -2,13 +2,12 @@ from django.http import JsonResponse
 from django.conf import settings
 import os
 
-def test_supabase_config(request):
-    """Test view to check Supabase configuration"""
+def test_database_config(request):
+    """Test view to check database configuration"""
     config = {
-        'supabase_url': settings.SUPABASE_URL,
-        'has_key': bool(settings.SUPABASE_KEY),
-        'has_jwt_secret': bool(settings.SUPABASE_JWT_SECRET),
-        'has_anon_key': bool(os.getenv('SUPABASE_ANON_KEY')),
+        'database_url': 'PostgreSQL RDS',
+        'has_connection': settings.DATABASES['default']['NAME'] != 'db.sqlite3',
+        'database_name': settings.DATABASES['default']['NAME'],
         'env_file_loaded': bool(os.getenv('DJANGO_SECRET_KEY')),
     }
     return JsonResponse(config) 
